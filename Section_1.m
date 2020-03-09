adecc5130755aaef199ea7b818ea2a97d2e20951
Bashton <35300968+Bashton19@users.noreply.github.com>
2020-03-09 21:53:27 +0000

    Assignment completed, with MapReduce
    Large dataset unavailable due to Github file size limit

--- Section_1.m
+++ Section_1.m
@@ -1,7 +1,6 @@
 % Section I: Data Summary, Understanding and Visualisation (30%)
 
 % Reads the dataset into the workspace
-
 clinical_data = xlsread('clinicalfeatures.xlsx');
 
 % Task 1 - Check for missing values
@@ -18,7 +17,8 @@ ismember checks for 1's (NaN) in the array, and returns a 0 or 1
 missing value
 %}
 
-ismember(1, isnan(clinical_data))
+% Checks for missing values in dataset
+ismember(1, isnan(clinical_data));
 
 % Task 2 - Summary statistics
 
@@ -28,53 +28,109 @@ ismember(1, isnan(clinical_data))
 healthy = clinical_data(1:52,1:9);
 cancer = clinical_data(53:116,1:9);
 
-% Computes summary statistics for each feature in the healthy group, and
-% puts them in a table
+% Computes summary statistics for each feature in the healthy group, and puts them in a table
 
 Variable = categorical(["Age";"BMI";"Glucose";"Insulin";"HOMA";"Leptin";"Adiponectin";"Resistin";"MCP.1"]); 
-Minimum = min(healthy)';
+Minimum = min(healthy)'; % Transposes row vector for each stat
 Maximum = max(healthy)';
 Mean = mean(healthy)';
 Median = median(healthy)';
 Variance = var(healthy)';
-t_health = table(Variable, Minimum, Maximum, Mean, Median, Variance);
+t_health = table(Variable, Minimum, Maximum, Mean, Median, Variance)
 
-% Computes summary statistics for each feature in the cancer group, and
-% puts them in a table
+% Computes summary statistics for each feature in the cancer group, and puts them in a table
 
 Variable = categorical(["Age";"BMI";"Glucose";"Insulin";"HOMA";"Leptin";"Adiponectin";"Resistin";"MCP.1"]); 
-Minimum = min(cancer)';
+Minimum = min(cancer)'; % Transposes row vector for each stat
 Maximum = max(cancer)';
 Mean = mean(cancer)';
 Median = median(cancer)';
 Variance = var(cancer)';
-t_cancer = table(Variable, Minimum, Maximum, Mean, Median, Variance);
+t_cancer = table(Variable, Minimum, Maximum, Mean, Median, Variance)
 
-% Creates boxplot of each feature in the healthy group
+% Creates boxplot of each feature for both groups
+x = [healthy(:,1); cancer(:,1)]; % Groups age columns, and groups varying sizes as one size
+g = [repmat({'Healthy Controls'}, size(healthy(:,1))); repmat({'Cancer Patients'}, size(cancer(:,1)))];
+boxplot(x, g) % Plots age values
+xlabel('Subjects') % Labels x axis
+ylabel('Age') % Labels y axis
+title('Age of Subjects') % Gives title
 
-age_1 = boxplot(healthy(:,1));
-bmi_1 = boxplot(healthy(:,2));
-glucose_1 = boxplot(healthy(:,3));
-insulin_1 = boxplot(healthy(:,4));
-homa_1 = boxplot(healthy(:,5));
-leptin_1 = boxplot(healthy(:,6));
-adipon_1 = boxplot(healthy(:,7));
-resistin_1 = boxplot(healthy(:,8));
-mcp_1 = boxplot(healthy(:,9));
-
-% Creates boxplot of each feature in the cancer group
-
-age_2 = boxplot(cancer(:,1));
-bmi_2 = boxplot(cancer(:,2));
-glucose_2 = boxplot(cancer(:,3));
-insulin_2 = boxplot(cancer(:,4));
-homa_2 = boxplot(cancer(:,5));
-leptin_2 = boxplot(cancer(:,6));
-adipon_2 = boxplot(cancer(:,7));
-resistin_2 = boxplot(cancer(:,8));
-mcp_2 = boxplot(cancer(:,9));
+% Repeats above process for other features
+figure()
+x = [healthy(:,2); cancer(:,2)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,2))); repmat({'Cancer Patients'}, size(cancer(:,2)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('BMI')
+title('BMI of Subjects')
+figure()
+x = [healthy(:,3); cancer(:,3)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,3))); repmat({'Cancer Patients'}, size(cancer(:,3)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('Glucose')
+title('Glucose Levels of Subjects')
+figure()
+x = [healthy(:,4); cancer(:,4)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,4))); repmat({'Cancer Patients'}, size(cancer(:,4)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('Insulin')
+title('Insulin Levels of Subjects')
+figure()
+x = [healthy(:,5); cancer(:,5)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,5))); repmat({'Cancer Patients'}, size(cancer(:,5)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('HOMA')
+title('HOMA of Subjects')
+figure()
+x = [healthy(:,6); cancer(:,6)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,6))); repmat({'Cancer Patients'}, size(cancer(:,6)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('Leptin')
+title('Leptin Levels of Subjects')
+figure()
+x = [healthy(:,7); cancer(:,7)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,7))); repmat({'Cancer Patients'}, size(cancer(:,7)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('Adiponectin')
+title('Adiponectin Levels of Subjects')
+figure()
+x = [healthy(:,8); cancer(:,8)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,8))); repmat({'Cancer Patients'}, size(cancer(:,8)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('Resistin')
+title('Resistin Levels of Subjects')
+figure()
+x = [healthy(:,9); cancer(:,9)];
+g = [repmat({'Healthy Controls'}, size(healthy(:,9))); repmat({'Cancer Patients'}, size(cancer(:,9)))];
+boxplot(x, g)
+xlabel('Subjects')
+ylabel('MCP.1')
+title('MCP.1 Levels of Subjects')
 
 % Task 3 - Correlation matrix
 
-corr_matrix = corrcoef(clinical_data(:,1:9));
+% Creates correlation matrix of clinical features
+corr_matrix = corrcoef(clinical_data(:,1:9))
+array2table(corr_matrix)
+
+% Creates correlation plot
+
+figure()
+corrplot(clinical_data(:,1:9))
+
+VariableNo = (['var1'; "var2"; "var3"; "var4"; "var5"; "var6"; "var7"; "var8"; "var9"]);
+Variable = (["Age"; "BMI"; "Glucose"; "Insulin"; "HOMA"; "Leptin"; "Adiponectin"; "Resistin"; "MCP.1"]);
+ 
+table(VariableNo, Variable)
+
+
+
+
 
--- Section_2.m
+++ Section_2.m
@@ -2,97 +2,164 @@
 
 % Task 4 - Split dataset into training and test
 
+% Splits data into training/test with a 70/30 split
+
 [m,n] = size(clinical_data);
 P = 0.70;
-idx = randperm(m);
-train_data = clinical_data(idx(1:round(P*m)),:); 
-test_data = clinical_data(idx(round(P*m)+1:end),:);
+idx = randperm(m); % Permutates
+train_data = clinical_data(idx(1:round(P*m)),:); % Gets 70% training
+test_data = clinical_data(idx(round(P*m)+1:end),:); % Gets 30% testing
 
-x_train = train_data(:,1:9);
-y_train = train_data(:,10);
+% Checks size of training and testing
+train_size = size(train_data);
+test_size = size(test_data);
+
+x_train = train_data(:,1:9); % Sets 1-9 columns as x
+y_train = train_data(:,10); % Sets classification column as y
 
 x_test = test_data(:,1:9);
 y_test = test_data(:,10);
+  
+train_healthy = 0; % Creates variables and sets to 0
+train_cancer = 0;
+
+for i = 1:numel(y_train) % Iterates through every value in y
+    if y_train(i) == 1;  % If 1, increment train_healthy
+        train_healthy = train_healthy + 1;
+    elseif y_train(i) == 2; % If 2, increment test_healthy
+        train_cancer = train_cancer + 1;
+    end
+end
+
+test_healthy=0;
+test_cancer = 0;
+
+for i = 1:numel(y_test)
+    if y_test(i) == 1;
+        test_healthy = test_healthy + 1;
+    elseif y_test(i) == 2;
+        test_cancer = test_cancer + 1;
+    end
+end
+
+% Print number of healthy and cancer for training and testing
+train_healthy;
+train_cancer;  
+test_healthy;
+test_cancer;
 
 % Task 5 - Train decision tree and SVM
 
-dt_train = fitctree(x_train, y_train);
+dt_train = fitctree(x_train, y_train); % Creates decision tree
+view(dt_train, 'mode', 'graph'); % Displays decision tree
 
-view(dt_train, 'mode', 'graph')
+svm_train = fitcsvm(x_train, y_train); % Creates SVM
 
-svm_train = fitcsvm(x_train, y_train);
+predict_dt = predict(dt_train, x_test); % Uses decision tree to classify x
+predict_svm = predict(svm_train, x_test); % Uses SVM classifier to classify x
 
-dt_test = fitctree (x_test, y_test);
+figure()
+conf_dt = confusionmat(y_test, predict_dt); % Creates confusion matrix
+confusionchart(conf_dt) % Plots matrix in a chart
+title('Decision Tree')
 
-view(dt_test, 'mode', 'graph')
+figure()
 
-svm_test = fitcsvm(x_test, y_test);
+conf_svm = confusionmat(y_test, predict_svm); % Creates confusion matrix
+confusionchart(conf_svm) % Plots matrix in a chart
+title('SVM')
 
-label_dt = predict(dt_test, x_test(30,:));
+% Sensitivity = TP/Actual Positives
+sens_dt = conf_dt(2,2)/sum(conf_dt(2,:));
+sens_svm = conf_svm(2,2)/sum(conf_svm(2,:));
 
-label_svm = predict(svm_test, x_test(30,:));
+% Specificty = TN/Actual Negatives
+spec_dt = conf_dt(1,1)/sum(conf_dt(1,:));
+spec_svm = conf_svm(1,1)/sum(conf_svm(1,:));
 
+% Error rate = (FP+FN)/Total
+error_dt = (conf_dt(2,1)+conf_dt(1,2))/sum(conf_dt, 'all');
+error_svm = (conf_svm(2,1)+conf_svm(1,2))/sum(conf_svm, 'all');
 
+% Table to show confusion metrics
+Metrics = categorical(["Sensitivity"; "Specificity"; "Error Rate"]);
+Decision_Tree = [sens_dt; spec_dt; error_dt];
+SVM = [sens_svm; spec_svm; error_svm];
+t_compare = table(Metrics, Decision_Tree, SVM)
 
 % Task 8
+ds = datastore('clinicalfeatures1.csv', 'TreatAsMissing', 'NA'); % Reads data in as a datastore
 
-ds = datastore('clinicalfeatures1.csv', 'TreatAsMissing', 'NA');
+mean_homa = mapreduce(ds, @mean_homa_mapper, @mean_homa_reducer); % Calls mapreduce function
+readall(mean_homa) % Prints mean
 
-mean_homa = mapreduce(ds, @mean_homa_mapper, @mean_homa_reducer);
-max_homa = mapreduce(ds, @max_homa_mapper, @max_homa_reducer);
-min_homa = mapreduce(ds, @min_homa_mapper, @min_homa_reducer);
+max_homa = mapreduce(ds, @max_homa_mapper, @max_homa_reducer); % Calls mapreduce function
+readall(max_homa) % Prints maximum
+min_homa = mapreduce(ds, @min_homa_mapper, @min_homa_reducer); % Calls mapreduce function
+readall(min_homa) % Prints minimum
 
-readall(mean_homa)
-readall(max_homa)
-readall(min_homa)
+var_homa = mapreduce(ds, @var_homa_mapper, @var_homa_reducer);
+readall(var_homa)
 
-function mean_homa_mapper(data, info, interm_kv_store)
-    homas = data.HOMA(~isnan(data.HOMA));
-    sum_len_value = [sum(homas) length(homas)];
-    add(interm_kv_store, 'sum and length', sum_len_value);
+function mean_homa_mapper(data, info, interm_kv_store) % Mean map function
+    homas = data.HOMA(~isnan(data.HOMA)); % Removes missing values
+    sum_len_value = [sum(homas) length(homas)]; % Finds count and sum of each block
+    add(interm_kv_store, 'sum_and_length', sum_len_value); % Added intermediate results
 end
-function mean_homa_reducer(interm_key, interm_val_iter, out_kv_store)
-    sum_len = [0 0];
-    while hasnext(interm_val_iter)
-        sum_len = sum_len + getnext(interm_val_iter);
+
+function mean_homa_reducer(interm_key, interm_val_iter, out_kv_store) % Mean reduce function
+    sum_len = [0 0]; % Sum and length declares in matrix
+    while hasnext(interm_val_iter) % Loops through each value
+        sum_len = sum_len + getnext(interm_val_iter); % Running total of distance and count after each pass
     end
-    add(out_kv_store, 'Mean HOMA', sum_len(1)/sum_len(2));
+    add(out_kv_store, 'Mean_HOMA', sum_len(1)/sum_len(2)); % Adds final key-value pair
 end
 
-function max_homa_mapper(data, info, interm_kv_store)
-    part_max = max(data.HOMA);
-    add(interm_kv_store, 'partial_maximum', part_max);
+function max_homa_mapper(data, info, interm_kv_store) % Maximum map function
+    part_max = max(data.HOMA); % Finds maximum of each block
+    add(interm_kv_store, 'partial_maximum', part_max); % Adds intermediate results
 end
 
-function max_homa_reducer(interm_key, interm_val_iter, out_kv_store)
-    max_val = -Inf;
-    while hasnext(interm_val_iter)
-        max_val=max(getnext(interm_val_iter), max_val);
+function max_homa_reducer(interm_key, interm_val_iter, out_kv_store) % Maximum reduce function
+    max_val = -Inf; % Declares maximum as negative infinity
+    while hasnext(interm_val_iter) % Loops through each value
+        max_val=max(getnext(interm_val_iter), max_val); % Finds overall maximum
     end
-    add(out_kv_store, 'Maximum HOMA',max_val);
+    add(out_kv_store, 'Maximum HOMA',max_val); % Adds final key-value pair
 end
 
-function min_homa_mapper(data, info, interm_kv_store)
-    part_min = min(data.HOMA);
-    add(interm_kv_store, 'partial_minimum', part_min);
+function min_homa_mapper(data, info, interm_kv_store) % Minimum map function
+    part_min = min(data.HOMA); % Finds minimum of each block
+    add(interm_kv_store, 'partial_minimum', part_min); % Adds intermediate results
 end
 
-function min_homa_reducer(interm_key, interm_val_iter, out_kv_store)
-    min_val = Inf;
-    while hasnext(interm_val_iter)
-        min_val=min(getnext(interm_val_iter), min_val);
+function min_homa_reducer(interm_key, interm_val_iter, out_kv_store) % Minimum reduce function
+    min_val = Inf; % Declares minimum as infinity
+    while hasnext(interm_val_iter) % Loops through each value
+        min_val=min(getnext(interm_val_iter), min_val); % Finds overall minimum
     end
-    add(out_kv_store, 'Minimum HOMA', min_val);
+    add(out_kv_store, 'Minimum HOMA', min_val); % Adds final key-value pair
 end
 
-function var_homa_mapper(data, info, interm_kv_store)
-    homas = data.HOMA(~isnan(data.HOMA));
-    sum_len_value = [sum(homas) length(homas)];
-    add(interm_kv_store, 'sum and length', sum_len_value);
+function var_homa_mapper(data, info, interm_kv_store) % Map function to calculate variance
+    homas = data.HOMA(~isnan(data.HOMA)); % Checks for NaN
+    sum_len_value = [sum(((homas)-1.6253).^2), length(homas)]; % Begins to calculate variance, and stores the length
+    add(interm_kv_store, 'sum and length', sum_len_value); % Adds intermediate results
 end
 
-function var_homa_reducer(interm_key, interm_val_iter, out_kv_store)
-    
+function var_homa_reducer(interm_key, interm_val_iter, out_kv_store) % Reduce function to calculate variance
+    sum = 0; % Declares sum as 0
+    length = 0; % Declares length as 0
+    while hasnext(interm_val_iter) % Loops through each value
+        sum_len_value2 = getnext(interm_val_iter); % Finds total length and sum
+        sum = sum + sum_len_value2(1); % Updates sum
+        length = length + sum_len_value2(2); % Updates length
+    end
+    variance = sum/length; % Divides sum of all numbers(-mean) by length
+    add(out_kv_store, 'HOMA Variance', variance) % Adds final key-value pair
+end
+
+
     
 
     
